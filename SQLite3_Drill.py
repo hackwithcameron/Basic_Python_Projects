88750f0b0d338acf6150db0a7fc319ed75fcdeb2
Cameron <hackwithcameron@gamil.com>
2019-11-15 17:09:37 -0800

    Create SQLite3_Drill.py
    Little drill that filters a list to return and append the results to a db.

---
+++ SQLite3_Drill.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+# conn = sqlite3.connect("txt_doc.db")
+fileList = ('information.docx', 'Hello.txt', 'myImage.png',
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+
+
+def find_txt(fileList):
+    lst_1 = []
+    for i in fileList:
+        if i[-1:-4:-1] == "txt":
+            lst_1.append(i)
+    return lst_1
+
+
+txt_lst = find_txt(fileList)
+
+
+def database():
+    conn = sqlite3.connect("doc.db")
+
+    with conn:  # Create db if not already created
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS tbl_txt_doc( \
+            ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+            file_names TEXT)")
+        conn.commit()
+    conn.close()
+
+
+def enter_data(txt_lst):
+    conn = sqlite3.connect("doc.db")
+
+    with conn:
+        cur = conn.cursor()
+        for i in range(len(txt_lst)):
+            cur.execute("INSERT INTO tbl_txt_doc(file_names) VALUES (?)", (txt_lst[i],))
+        conn.commit()
+    conn.close()
+
+
+def get_txt():
+    conn = sqlite3.connect("doc.db")
+
+    with conn:  # Getting values from db
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM tbl_txt_doc")
+        var = cur.fetchall()
+        for i in (range(len(var))):
+            msg = "Text file: {}".format(txt_lst[i])
+            print(msg)
+    conn.close()
+
+
+if __name__ == "__main__":
+    database()
+    enter_data(txt_lst)
+    get_txt()
